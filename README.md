8272a27f496151fc0a866b136a455aecbe779d3f
Alexandre Miranda Rezende Rocha <alexandremrr2014@gmail.com>
2023-07-30 22:30:11 -0300

    FEAT: Biblioteca de busca de cep em php

---
+++ README.md
@@ -0,0 +1,9 @@
+# Biblioteca Busca CEP
+
+Uma biblioteca criada em PHP para busca de cep online utilizando a API do IBGE
+
+## Aprendizados
+
+- PHP
+- Composer
+
---
+++ src/Search.php
@@ -0,0 +1,17 @@
+<?php
+
+    namespace Alexandre\DigitalCep;
+
+    class Search{
+        
+        private $url = "https://viacep.com.br/ws/";
+
+        public function getAddressFromZipcode(string $zipCode) {
+            
+            $zipCode = preg_replace('/^0-9]/im', '', $zipCode);
+            $get = file_get_contents($this->url . $zipCode . "/json");
+
+            return ($get);
+        }
+    }
+?>
