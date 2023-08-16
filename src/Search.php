0ebc2ecca39ff13abed4fed3a36a712d69a36a6f
Alexandre Miranda Rezende Rocha <alexandremrr2014@gmail.com>
2023-08-15 21:37:44 -0300

    FEAT: usando o padrão PSR12 no PHP

--- src/Search.php
+++ src/Search.php
@@ -2,16 +2,14 @@
 
     namespace Alexandre\DigitalCep;
 
-    class Search{
-        
-        private $url = "https://viacep.com.br/ws/";
+class Search{
+    private $url = "https://viacep.com.br/ws/";
 
-        public function getAddressFromZipcode(string $zipCode) {
-            
-            $zipCode = preg_replace('/^0-9]/im', '', $zipCode);
-            $get = file_get_contents($this->url . $zipCode . "/json");
+    public function getAddressFromZipcode(string $zipCode){
 
-            return (json_decode($get));
-        }
+        $zipCode = preg_replace('/^0-9]/im', '', $zipCode);
+        $get = file_get_contents($this->url . $zipCode . "/json");
+
+        return (json_decode($get));
     }
-?>
+}
